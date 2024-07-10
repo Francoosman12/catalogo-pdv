7520c137d497ebb05458395f0869daffcc0bfecf
Franco Osman <francoosman12@gmail.com>
2024-07-10 12:08:29 -0300

    prueba

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import logo from './assets/Logo_PDV-removebg-preview.png'; // Ajusta la ruta según la ubicación exacta
 
 const Navbar = ({ proveedores, proveedorSeleccionado, setProveedorSeleccionado }) => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="/">Catálogo</a>
+            <a className="navbar-brand" href="/">
+                <img src={logo} alt="Logo de la empresa" width="auto" height="30" />
+            </a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
